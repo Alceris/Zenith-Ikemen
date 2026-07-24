ecbbc723628f085f8a453f8a69a81ce8da239e10
Alceris <nightmare334@gmail.com>
2026-07-23 21:13:05 -0800

    Add Zenith Input
    Custom input buffer system, may document later.

---
+++ ZenithInput/ZenithInput.cmd
@@ -0,0 +1,63 @@
+[Command]
+name = "ZenithInputL"
+command = /L
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputDL"
+command = /DL
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputD"
+command = /D
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputDR"
+command = /DR
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputR"
+command = /R
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputUL"
+command = /UL
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputU"
+command = /U
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
+
+[Command]
+name = "ZenithInputUR"
+command = /UR
+time = 0
+buffer.time = 1
+buffer.hitpause = 0
+buffer.pauseend = 0
